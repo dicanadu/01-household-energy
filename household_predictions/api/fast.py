cf6904d31deddac1333b8f328eeb933ee540b0a3
Svetlana Lebedeva <slebedeva@gmail.com>
2023-11-30 11:52:00 +0100

    checkpoint local api

--- household_predictions/api/fast.py
+++ household_predictions/api/fast.py
@@ -20,39 +20,40 @@ app.add_middleware(
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 @app.get("/predict")
-def predict(
-    TYPEHUQ: int,
-    STORIES: int,
-    YEARMADERANGE: int,
-    WALLTYPE: int,
-    ROOFTYPE: int,
-    WINDOWS: int,
-    SWIMPOOL: int,
-    DISHWASH: int,
-    CWASHER: int,
-    DRYER: int,
-    TELLWORK: int,
-    TELLDAYS: int,
-    HEATHOME: int,
-    EQUIPM: int,
-    NUMPORTEL: int,
-    AIRCOND: int,
-    NUMPORTAC: int,
-    SMARTMETER: int,
-    SOLAR: int,
-    NCOMBATH: int,
-    NHAFBATH: int,
-    TOTROOMS: int,
-    NUMFRIG: int,
-    MICRO: int,
-    TVCOLOR: int,
-    DESKTOP: int,
-    NUMLAPTOP: int,
-    LGTIN1TO4: int,
-    LGTIN4TO8: int,
-    LGTINMORE8: int,
-    NHSLDMEM: int,
-    SQFTEST: int):
+def predict(**params
+    #TYPEHUQ: int,
+    #STORIES: int,
+    #YEARMADERANGE: int,
+    #WALLTYPE: int,
+    #ROOFTYPE: int,
+    #WINDOWS: int,
+    #SWIMPOOL: int,
+    #DISHWASH: int,
+    #CWASHER: int,
+    #DRYER: int,
+    #TELLWORK: int,
+    #TELLDAYS: int,
+    #HEATHOME: int,
+    #EQUIPM: int,
+    #NUMPORTEL: int,
+    #AIRCOND: int,
+    #NUMPORTAC: int,
+    #SMARTMETER: int,
+    #SOLAR: int,
+    #NCOMBATH: int,
+    #NHAFBATH: int,
+    #TOTROOMS: int,
+    #NUMFRIG: int,
+    #MICRO: int,
+    #TVCOLOR: int,
+    #DESKTOP: int,
+    #NUMLAPTOP: int,
+    #LGTIN1TO4: int,
+    #LGTIN4TO8: int,
+    #LGTINMORE8: int,
+    #NHSLDMEM: int,
+    #SQFTEST: int
+    ):
     """
      Make a prediction based on user inputs.
      Baseline model is the default and is pulled from the cloud.
@@ -65,6 +66,7 @@ def predict(
     ## make a dataframe
     ## values of the dict should be lists
     X_new = pd.DataFrame({k:[v] for k,v in user_inputs.items()})
+    #print(f'###### new X: \n {X_new.head()}')
 
     ## clean the dataframe
     X_new_clean = clean_data(X_new)
@@ -74,6 +76,7 @@ def predict(
     #X_train = clean_data(call_data_url()).drop(columns = 'KWH')
     #preprocessor = preprocessing(X_train)
     #X_new_processed = preprocessor.transform(X_new_clean)
+
     y_pred = app.state.model.predict(X_new_clean)
 
     return {'kwh_prediction': int(y_pred)}
