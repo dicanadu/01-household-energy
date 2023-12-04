c4e23e59ad520b66ff19f322cb580bb40a1111d8
rkemp101 <rkemp101@gmail.com>
2023-12-04 19:42:58 +0100

    modified timer for progress bar

--- streamlit_app/pages/0_main.py
+++ streamlit_app/pages/0_main.py
@@ -268,7 +268,7 @@ with st.sidebar:
             my_bar = st.progress(0, text=progress_text)
 
             for percent_complete in range(100):
-                time.sleep(0.01)
+                time.sleep(0.05)
                 my_bar.progress(percent_complete + 1, text=progress_text)
             time.sleep(1.5)
 
