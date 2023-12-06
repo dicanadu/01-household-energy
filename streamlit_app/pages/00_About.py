845f80a14a5701dd6ce5004893e58356e750bdb8
Svetlana Lebedeva <slebedeva@gmail.com>
2023-12-06 16:34:38 +0100

    added csv with examples for API usage in About

--- streamlit_app/pages/00_About.py
+++ streamlit_app/pages/00_About.py
@@ -103,9 +103,97 @@ Call our API and try it yourself !!!!
 : )
 ''')
 
-variable = ['TYPEHUQ','NHSLDMEM','state_name','BA_climate','SQFTEST','STORIES','YEARMADERANGE','NCOMBATH','NHAFBATH','TOTROOMS','WINDOWS','SWIMPOOL','SMARTMETER','DESKTOP','NUMLAPTOP','TVCOLOR','DISHWASH','MICRO','NUMFRIG','CWASHER','DRYER','LGTIN1TO4','LGTIN4TO8','LGTINMORE8','AIRCOND','EQUIPM','HEATHOME','NUMPORTEL','PRICEKWH']
-description = ['Type of housing unit','Number of household members (top-coded)','State Name','Building America Climate Zone','Respondent-reported square footage (rounded to the nearest 10)','Number of stories in a single-family home','Range when housing unit was built','Number of full bathrooms (top-coded)','Number of half bathrooms (top-coded)','Total number of rooms in the housing unit, excluding bathrooms; a derived variable','Number of windows','Has swimming pool','Home has an electricity smart meter','Number of desktop computers used','Number of laptop computers used','Number of televisions used','Has dishwasher','Number of microwaves','Number of refrigerators used','Has clothes washer in home','Has clothes dryer in home','Number of inside light bulbs turned on 1 to 4 hours per day','Number of inside light bulbs turned on 4 to 8 hours per day','Number of inside light bulbs turned on more than 8 hours per day','Air conditioning equipment used','Main space heating equipment type','Space heating equipment used','Number of portable electric heaters used','Price paid per kWh']
-variable_df = pd.DataFrame({"variable": variable, "description": description}).set_index("variable")
+#variable = ['TYPEHUQ','NHSLDMEM','state_name','BA_climate','SQFTEST','STORIES','YEARMADERANGE','NCOMBATH','NHAFBATH','TOTROOMS','WINDOWS','SWIMPOOL','SMARTMETER','DESKTOP','NUMLAPTOP','TVCOLOR','DISHWASH','MICRO','NUMFRIG','CWASHER','DRYER','LGTIN1TO4','LGTIN4TO8','LGTINMORE8','AIRCOND','EQUIPM','HEATHOME','NUMPORTEL','PRICEKWH']
+#description = ['Type of housing unit','Number of household members (top-coded)','State Name','Building America Climate Zone','Respondent-reported square footage (rounded to the nearest 10)','Number of stories in a single-family home','Range when housing unit was built','Number of full bathrooms (top-coded)','Number of half bathrooms (top-coded)','Total number of rooms in the housing unit, excluding bathrooms; a derived variable','Number of windows','Has swimming pool','Home has an electricity smart meter','Number of desktop computers used','Number of laptop computers used','Number of televisions used','Has dishwasher','Number of microwaves','Number of refrigerators used','Has clothes washer in home','Has clothes dryer in home','Number of inside light bulbs turned on 1 to 4 hours per day','Number of inside light bulbs turned on 4 to 8 hours per day','Number of inside light bulbs turned on more than 8 hours per day','Air conditioning equipment used','Main space heating equipment type','Space heating equipment used','Number of portable electric heaters used','Price paid per kWh']
+#variable_df = pd.DataFrame({"variable": variable, "description": description}).set_index("variable")
+#variable_df.index.name = None
+#
+## Set Streamlit option to display all rows
+#user_input = {
+#  "TYPEHUQ": 2,
+#  "SQFTEST": 1530,
+#  "NHSLDMEM": 2,
+#  "state_name": "California",
+#  "PRICEKWH": 0.2999,
+#  "BA_climate": "Cold",
+#  "TOTROOMS": 6,
+#  "STORIES": 1,
+#  "YEARMADERANGE": 4,
+#  "HEATHOME": 1,
+#  "SMARTMETER": 0,
+#  "NCOMBATH": 2,
+#  "NHAFBATH": 0,
+#  "EQUIPM": 3,
+#  "WINDOWS": 4,
+#  "SWIMPOOL": 0,
+#  "DESKTOP": 0,
+#  "NUMLAPTOP": 0,
+#  "MICRO": 1,
+#  "NUMPORTEL": 0,
+#  "CWASHER": 1,
+#  "AIRCOND": 1,
+#  "TVCOLOR": 2,
+#  "NUMFRIG": 1,
+#  "LGTIN1TO4": 4,
+#  "LGTIN4TO8": 0,
+#  "LGTINMORE8": 0,
+#  "DRYER": 1,
+#  "DISHWASH": 1
+#}
+#df_2 = pd.DataFrame(user_input, index = [0]).T
+#converged_pd = pd.merge(variable_df, df_2, left_index=True, right_index=True)
+#converged_pd.columns = ["Description", "Example"]
+#
+#st.dataframe(converged_pd)
+#
+
+variable = ['TYPEHUQ', 'NHSLDMEM', 'state_name', 'BA_climate', 'SQFTEST',
+       'STORIES', 'YEARMADERANGE', 'NCOMBATH', 'NHAFBATH', 'TOTROOMS',
+       'WINDOWS', 'SWIMPOOL', 'SMARTMETER', 'DESKTOP', 'NUMLAPTOP',
+       'TVCOLOR', 'DISHWASH', 'MICRO', 'NUMFRIG', 'CWASHER', 'DRYER',
+       'LGTIN1TO4', 'LGTIN4TO8', 'LGTINMORE8', 'AIRCOND', 'EQUIPM',
+       'HEATHOME', 'NUMPORTEL']
+description = ['Type of housing unit', 'Number of household members (top-coded)',
+       'State Name', 'Building America Climate Zone',
+       'Respondent-reported square footage (rounded to the nearest 10)',
+       'Number of stories in a single-family home',
+       'Range when housing unit was built',
+       'Number of full bathrooms (top-coded)',
+       'Number of half bathrooms (top-coded)',
+       'Total number of rooms in the housing unit, excluding bathrooms; a derived variable',
+       'Number of windows', 'Has swimming pool',
+       'Home has an electricity smart meter',
+       'Number of desktop computers used',
+       'Number of laptop computers used', 'Number of televisions used',
+       'Has dishwasher', 'Number of microwaves',
+       'Number of refrigerators used', 'Has clothes washer in home',
+       'Has clothes dryer in home',
+       'Number of inside light bulbs turned on 1 to 4 hours per day',
+       'Number of inside light bulbs turned on 4 to 8 hours per day',
+       'Number of inside light bulbs turned on more than 8 hours per day',
+       'Air conditioning equipment used',
+       'Main space heating equipment type',
+       'Space heating equipment used',
+       'Number of portable electric heaters used']
+data_type = [' int', ' int', ' str', ' str', ' int', ' int', ' int', ' int',
+       ' int', ' int', ' int', ' int', ' int', ' int', ' int', ' int',
+       ' int', ' int', ' int', ' int', ' int', ' int', ' int', ' int',
+       ' int', ' int', ' int', ' int']
+example = ['1 Mobile home\n2 Single-family house detached from any other house \n3 Single-family house attached to one or more other houses (for example: duplex, row house, or townhome)\n4 Apartment in a building with 2 to 4 units\n5 Apartment in a building with 5 or more units',
+       '1 - 7', 'state_dictionary!A1',
+       'Cold\nHot-Dry\nHot-Humid\nMarine\nMixed-Dry\nMixed-Humid\nSubarctic\nVery-Cold',
+       '240-15000',
+       '1 One story\n2 Two stories\n3 Three stories\n4 Four or more stories\n5 Split-level\n-2 Not applicable',
+       '1 Before 1950\n2 1950 to 1959\n3 1960 to 1969\n4 1970 to 1979\n5 1980 to 1989\n6 1990 to 1999\n7 2000 to 2009\n8 2010 to 2015\n9 2016 to 2020',
+       '0 - 4', '0 - 2', '1-15',
+       '1 1 or 2 windows\n2 3 to 5 windows\n3 6 to 9 windows\n4 10 to 15 windows\n5 16 to 19 windows\n6 20 to 29 windows\n7 30 or more windows',
+       '1 Yes\n0 No\n-2 Not applicable', "1 Yes\n0 No\n-4 Don't Know",
+       '0 - 8', '0 - 20', '0 - 14', '1 Yes\n0 No', '0 - 3', '0 - 9',
+       '1 Yes\n0 No', '1 Yes\n0 No', '0 - 90', '0 - 84', '0 - 99',
+       '1 Yes\n0 No',
+       '3 Central furnace \n2 Steam or hot water system with radiators or pipes \n4 Central heat pump\n13 Ductless heat pump, also known as a "mini-split"\n5 Built-in electric units installed in walls, ceilings, baseboards, or floors\n7 Built-in room heater burning gas or oil\n8 Wood or pellet stove \n10 Portable electric heaters\n99 Other \n-2 Not applicable',
+       '1 Yes\n0 No', '1 - 9\n-2 Not applicable']
+variable_df = pd.DataFrame({"variable": variable, "description": description, 'data_type': data_type, 'codes': example}).set_index("variable")
 variable_df.index.name = None
 
 # Set Streamlit option to display all rows
@@ -142,6 +230,6 @@ user_input = {
 }
 df_2 = pd.DataFrame(user_input, index = [0]).T
 converged_pd = pd.merge(variable_df, df_2, left_index=True, right_index=True)
-converged_pd.columns = ["Description", "Example"]
+converged_pd.columns = ["Description", "Data Type", "Codes", "Example"]
 
 st.dataframe(converged_pd)
